8c81a08dce25abcd6656dd3bf67ed069c6637761
Pzsombor <Pap.Zsombor@C19-01>
2026-09-09 08:18:43 +0200

    Add project files.

---
+++ MethodsAndClasses/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MethodsAndClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
