118657cb378dfbcf27f0e57cf4b5d284ff5a9f0b
Pzsombor <Pap.Zsombor@C19-01>
2026-09-09 09:41:53 +0200

    1.rész

--- MethodsAndClasses/Book.cs
+++ MethodsAndClasses/Book.cs
@@ -6,7 +6,70 @@ using System.Threading.Tasks;
 
 namespace MethodsAndClasses
 {
-    internal class Book
+    public class Book
     {
+        public static int Count = 0;
+        private int pageCount { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PageCount { get { return pageCount; } set { if (value < 0) { pageCount = 0; } else { pageCount = value; } } }
+        public bool isAvailable { get; set; }
+
+        public Book(string title, string author, int pageCount) : this(title, author)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.pageCount = pageCount;
+            Count++;
+            this.isAvailable = true; ;
+        }
+        public Book(string title, string author)
+        {
+            this.pageCount = 0;
+        }
+
+        public void Describe()
+        {
+            Console.WriteLine($"Cím:{Title},szerző: {Author},oldalszám:{pageCount}");
+        }
+        public bool IsLong()
+        {
+            if (pageCount > 300)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Borrow()
+        {
+            if (isAvailable)
+            {
+                isAvailable = false;
+                Console.WriteLine("kikölcsönözted");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Return()
+        {
+            if (isAvailable = false)
+            {
+                isAvailable = true;
+                Console.WriteLine("visszahozta");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
     }
 }
--- MethodsAndClasses/Program.cs
+++ MethodsAndClasses/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Book book1 = new Book("Bogyó és Babóca", "nincs infó ", 300);
+            Book book2 = new Book("Harry Potter", "nincs infóm", -200);
+            Console.WriteLine(book1.Title,book2.Title);
+
+            book1.Describe();
+            book2.Describe();
+            book1.IsLong();
+            book2.IsLong();
+
+            Book book3 = new Book("A kis herceg", "nincs infó");
+            book2.Borrow();
+
         }
     }
 }
